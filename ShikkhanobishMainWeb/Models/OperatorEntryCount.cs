d33163d33d1d3e4e412df4cc0a0c532791924744
shikkhanobish <shikkhanobish.github@gmail.com>
2022-04-23 16:21:36 +0600

    Adding bindings

---
+++ ShikkhanobishMainWeb/Models/OperatorEntryCount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShikkhanobishMainWeb.Models
+{
+    public class OperatorEntryCount
+    {
+        public int operatorID { get; set; }
+        public int UploadCount { get; set; }
+        public int ApprovedCount { get; set; }
+        public int DeclineCount { get; set; }
+        public int PendingCount { get; set; }
+        public int Type { get; set; }
+        public string Response { get; set; }
+    }
+}
